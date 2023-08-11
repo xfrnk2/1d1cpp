565a6bf201cf7a81301bf635068aadbb44b3139d
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-08-11 23:06:03 +0900

    solve: 10870 피보나치수 5

---
+++ src/boj/p10k/p10800/P10870.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+		return n == 0 ? 0 : n == 1 ? 1 : fibo(n - 1) + fibo(n - 2);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fibo(n);
+}
