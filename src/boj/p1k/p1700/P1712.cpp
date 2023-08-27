e24395a4c023e6264defd8a787cdcde0c9b28dde
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-08-27 22:41:53 +0900

    solve: 1712 손익분기점

---
+++ src/boj/p1k/p1700/P1712.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;	
+    cin >> a >> b >> c;
+	if (c <= b) {
+		cout << "-1";
+        return 0;
+    }
+    cout << a / (c - b) + 1;
+}
