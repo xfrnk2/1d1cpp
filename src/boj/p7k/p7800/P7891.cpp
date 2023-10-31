7364940adb57d45c85a30de11d10e8f4f2408501
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-10-31 22:00:58 +0700

    solve: 7891 Can you add this?

---
+++ src/boj/p7k/p7800/P7891.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int t, a, b;
+
+int main(void){
+    cin >> t;
+    while(t--){
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
