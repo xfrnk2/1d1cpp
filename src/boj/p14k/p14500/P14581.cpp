55b63bf17c868830a7260d5703fccf73633aabc4
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-11-09 22:13:44 +0900

    solve: 14581 팬들에게 둘러싸인 홍준

---
+++ src/boj/p14k/p14500/P14581.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+string hongjun, fan = ":fan:";
+int main(void) {
+
+	cin >> hongjun;
+
+	cout << fan << fan << fan << endl;
+
+	cout << fan << ':' << hongjun << ':' << fan << endl;
+
+	cout << fan << fan << fan << endl;
+}
