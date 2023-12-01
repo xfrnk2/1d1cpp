8d7f03f58679d646c73b710d17ecc7fab6a33e34
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-12-02 00:44:55 +0900

    solve: 27889 특별한 학교 이름

---
+++ src/boj/p27k/p27800/P27889.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main(void) {
+  cin >> s;
+  if (s == "KIS")
+    cout << "Korea International School";
+  else if (s == "BHA")
+    cout << "Branksome Hall Asia";
+  else if (s == "NLCS")
+    cout << "North London Collegiate School";
+  else
+    cout << "St. Johnsbury Academy";
+}
