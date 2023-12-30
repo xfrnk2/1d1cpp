988c503d22830ded56e610be6322958a882477e2
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-12-31 00:08:18 +0900

    solve: 26574 Copier

---
+++ src/boj/p26k/p26500/P26574.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, num;
+int main(void) {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> num;
+    cout << num << " " << num << "\n";
+  }
+}
