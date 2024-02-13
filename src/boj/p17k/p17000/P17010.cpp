f2ba64385d58c996ac38a931c7fcad897e327561
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2024-02-14 00:26:55 +0900

    solve: 17010 Time To Decompress

---
+++ src/boj/p17k/p17000/P17010.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+char c;
+
+int main(void){
+  cin >> t;
+  while(t--> 0){
+    cin >> n >> c;
+    while(n--> 0) {
+        cout << c;
+    }
+    cout << endl;
+  }
+}
