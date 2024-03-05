166a9e12f92770ef5daeb9f2155fdf9c692236d1
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2024-03-06 01:20:03 +0900

    solve: 23235 The Fastest Sorting Algorithm In The World

---
+++ src/boj/p23k/p23200/P23235.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+    int i = 1;
+    while(true){
+        getline(cin, s);
+        if(s == "0") break;
+        printf("Case %d: Sorting... done!\n", i);
+        i++;
+    }
+}
