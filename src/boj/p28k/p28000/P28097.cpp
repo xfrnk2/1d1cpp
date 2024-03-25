44244d94249ea9b690ed0ae8af3b2fff1d00370b
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2024-03-25 22:21:46 +0900

    solve: 28097 모범생 포닉

---
+++ src/boj/p28k/p28000/P28097.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int n, t, ans = 0;
+int main(void) {
+    cin >> n;
+    ans += (n - 1) * 8;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        ans += t;
+    }
+    cout << ans / 24 << " " << ans % 24;
+}
