1b1dda16d58f69becd623fe35bf2333656304295
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2024-05-02 00:47:33 +0900

    solve: 28701 세제곱의 합

---
+++ src/boj/p28k/P28701.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main(void) {
+  cin >> n;
+  cout << (n + 1) * n / 2 << '\n';
+  cout << (int)pow((n + 1) * n / 2, 2) << '\n';
+  cout << (int)pow((n + 1) * n / 2, 2) << '\n';
+}
